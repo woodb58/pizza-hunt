ae49896f3a2b3c80f37f623696a30481d7deff0a
woodb58 <woodb58@gmail.com>
2022-06-22 15:56:49 -0500

    1

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,7 +4,13 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
       Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
           console.log(err);
           res.status(400).json(err);
@@ -14,18 +20,22 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
       Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
     },
     // createPizza
 createPizza({ body }, res) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,39 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
     },
     createdBy: {
-      type: String
+      type: String,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     size: {
       type: String,
-      default: 'Large'
+      default: "Large",
     },
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-  // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+// create the Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
