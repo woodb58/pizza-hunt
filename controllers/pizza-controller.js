77203a7f706f61f862d6febdc2e96ffa2dd6381e
woodb58 <woodb58@gmail.com>
2022-06-24 14:42:14 -0500

    added validators

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -45,8 +45,8 @@ createPizza({ body }, res) {
   },
 
   // update pizza by id
-updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
